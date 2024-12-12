9f0bbc8ccaf0ea9e76b5b655f671fc4ba34b2dc2
Matias Paavilainen <matias.paavilainen@gmail.com>
2024-12-12 11:10:20 +0200

    Comment

--- board/code.py
+++ board/code.py
@@ -9,32 +9,34 @@ import time
 chunkSize = 1024
 pool = socketpool.SocketPool(wifi.radio)
 
+
 def sendImage(pathToImage):
     # Image has to be sent in chunks, probably library limitation, only 2kb file worked during testing
     # Calculate the number of chunks needed for a given file
     imageSize = os.stat(pathToImage)[6]
     mqtt_client.publish("imageSize", imageSize)
     mqtt_client.publish("text", "amogus")
-    
-    numberOfChunks = int(imageSize/chunkSize + 1)
-    
+
+    numberOfChunks = int(imageSize / chunkSize + 1)
+
     # Send number of chunks to tell JavaScript how many chunks are coming
     mqtt_client.publish("chunkSize", numberOfChunks)
-    
+
     with open(pathToImage, "rb") as file:
         chunk = file.read(chunkSize)
         while chunk:
             b = bytes(chunk)
             mqtt_client.publish("images", b)
             chunk = file.read(chunkSize)
-    
+
     print(f"Image sent as {numberOfChunks} chunks")
 
 
 mqtt_client = MQTT.MQTT(
-    broker="192.168.187.236",
+    # Broker IP has to be set every time the network is reset
+    broker="",
     port=1883,
-    socket_pool=pool
+    socket_pool=pool,
 )
 
 mqtt_client.connect()
@@ -42,5 +44,3 @@ mqtt_client.connect()
 print("Connected to MQTT broker.")
 
 sendImage("GeZTI9XW0AAL3OY.jpg")
-
-
